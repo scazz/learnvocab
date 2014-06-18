0307cf7ba88b0e4df3d591e8c952663e315d2965
Scazz <sam@samcarr.me.uk>
2014-06-18 19:16:48 +0100

    nicer deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,14 +1,24 @@
+logger.level = Logger::DEBUG
+
 set :application, "LearnVocab"
 set :domain,      "#{application}.net"
 set :deploy_to,   "/www/#{domain}"
 set :app_path,    "app"
 
 set :repository,  "https://github.com/scazz/learnvocab.git"
+set :user,        "scazz"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set   :use_sudo,      false
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,   [app_path + "/logs", "vendor"]
+set :use_composer, true
+set :update_vendors, false
+set :dump_assetic_assets, true
 
 set :model_manager, "doctrine"
-# Or: `propel`
+
+
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
